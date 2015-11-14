fca85af6ab2919f6e1d9aecf470c608059cea96a
Joshua Sparks <JoshuaTaylorBW@gmail.com>
2015-11-14 12:12:03 -0700

    Transfer From NINJAMUSICIAN

---
+++ _06_personal-project/Website/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Creativus | Home</title>
+	<link rel="stylesheet" type="text/css" href="Home.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<div id="BannerSpacer"></div>
+	<div id="Banner">
+		<ul id="BannerSections">
+			<li id="CompanyNameTopLeft" onclick="location.href=index.php"><a id="CompanyText" href="index.php">Josh S.</a></li>
+			<li id="AboutLink"><a href="About.html" id="AboutText">About</a></li>
+		</ul>
+	</div>
+
+	<div id="Background-Wrap">
+		<div id="Bg">
+			<img src="Colorful.png" alt=""> 
+		</div>
+	</div>
+
+	<!--TODO Social Media Buttons-->
+</head>
+<body>
+	<div id="BlackBox">
+	</div>
+
+	
+	<p id="CompanySlogan" align="center">Hello! My name is Josh Sparks, <br> I spend my free time with friends <br> and working on side projects. 
+	<br> My best friend is my little sister, Natlaya. <br> I want to be an entrepreneur as an adult.</p>
+	
+</body>
+</html>
